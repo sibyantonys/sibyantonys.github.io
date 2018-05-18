12a598e8c24020740c8be2bf9df4d4571800bdf6
jose.tharayil <jose.tharayil@cabotsolutions.com>
2018-05-18 19:54:17 +0530

    init commit

---
+++ js/map.js
@@ -0,0 +1,14 @@
+function myMap() {
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {
+    center: new google.maps.LatLng(9.981505499999999, 76.2822238),
+    zoom: 20
+  };
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+  var markerOptions = {
+    icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|FF0000',
+    position: {lat: 9.981505499999999, lng: 76.2822238},
+    map:map
+  }
+  var beachMarker = new google.maps.Marker(markerOptions);
+}
